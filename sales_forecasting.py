84ccd356108d51355e14155d6a1dd59ddaaa767d
alee2799 <91075241+alee2799@users.noreply.github.com>
2024-04-29 18:49:03 -0400

    Update sales_forecasting.py
    added imported libraries and started the data preprocessing fucntion

--- sales_forecasting.py
+++ sales_forecasting.py
@@ -1,1 +1,8 @@
+import pandas as pd
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import accuracy_score
 
+def data_preprocess(file)
+  
