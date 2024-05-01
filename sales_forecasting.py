2b2d0b2be437833b4fc08331fc1b156eb615e73d
Patel, Ravi, R <71032349+FriedRiceTera2@users.noreply.github.com>
2024-05-01 18:09:42 -0400

    Update sales_forecasting.py

--- sales_forecasting.py
+++ sales_forecasting.py
@@ -18,5 +18,12 @@ transactions_csv = pd.read_csv('')
 holidays_events_csv = pd.read_csv('')
 oil_csv = pd.read_csv('')
 
+
+# Implementation for merging datasets
+# combine the features in stores.csv(city, state), oil.csv(oil prices), and holiday_events.csv(holiday date, type, locale, locale name), transactions.csv (transactions) into train.csv, test.csv, valid.csv
+# use the store_nbr found in stores.csv, and transactions.csv to connect it to train/test.csv/valid.csv
+# use the date attribute found in oil.csv and holiday.csv and connect it to train/test.csv/valid.csv.
+
+We will use the store_nbr found in stores.csv, and transactions.csv to connect it to train/test.csv. Then we will use the date attribute found in oil.csv and holiday.csv and connect it to train/test.csv/valid.csv.
 def data_preprocess():
     
