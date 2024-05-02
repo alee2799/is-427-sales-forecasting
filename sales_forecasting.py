888d8788aa3d74346df587f9bc8dffe920b559df
alee2799 <91075241+alee2799@users.noreply.github.com>
2024-05-02 17:09:58 -0400

    Update sales_forecasting.py

--- sales_forecasting.py
+++ sales_forecasting.py
@@ -8,19 +8,7 @@ from sklearn.ensemble import RandomForestClassifier
 from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import accuracy_score
 
-train = pd.read_csv('test.csv')
-test = pd.read_csv('train.csv')
-stores = pd.read_csv('stores.csv')
-transactions = pd.read_csv('transactions.csv')
-holidays = pd.read_csv('holidays_events.csv')
-oil_prices = pd.read_csv('oil.csv')
-
-#adds columns to training and test sets; also matches columns by date
-[train, stores, oilprices, holidays, ]
-def combine_data(dataset)
-    for d in dataset:
-
-#Implementation for merging datasets
+#Implementation for preprocessing datasets
 #combine the features in stores.csv(city, state), oil.csv(dcoilwtico), 
 #and holiday_events.csv(holiday date, type, locale, locale name), 
 #transactions.csv (transactions) into train.csv, test.csv, valid.csv
@@ -28,13 +16,26 @@ def combine_data(dataset)
 #to connect it to train/test.csv/valid.csv
 # use the date attribute found in oil.csv and holiday.csv 
 #and connect it to train/test.csv/valid.csv.
+
+#reads files and combines them together then returns the created data set
+def combine_data(files):
+    data = pd.concat([pd.read_csv(f) for f in files], ignore_index=True)
+    return data
+
+train = combine_data(['train.csv', 'stores.csv', 'oil.csv', 'holidays_events.csv', 'transactions.csv'])
+test = combine_data(['test.csv', 'stores.csv', 'oil.csv', 'holidays_events.csv', 'transactions.csv'])
+train.info()
+
 def preprocess(data, cols):
-    #checks if there is an array of columns to remove then removes them
+    #checks if there is an array of columns to remove then removes them from dataset
     if cols:
         data = data.drop(cols, axis=1)
     #removes rows with empty values
     data = data.dropna(how='any')
     return data
     
-train = preprocess(train, [])
-train.info()
+train = preprocess(train, ['type', 'cluster', 'description', 'transferred'])
+test = preprocess(test, ['type', 'cluster', 'description', 'transferred'])
+    
+#creates a valid set from splitting test set    
+#def split_sets()
